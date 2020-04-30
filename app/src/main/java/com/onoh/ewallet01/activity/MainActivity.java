56e6bfb4066cb1c785b001eceb5050666df1e4a4
daffamachfud <daffamachfud@gmail.com>
2020-05-01 03:13:37 +0700

    Implement tampilan menu utama (bottom navigation dan lainlain)

--- app/src/main/java/com/onoh/ewallet01/activity/MainActivity.java
+++ app/src/main/java/com/onoh/ewallet01/activity/MainActivity.java
@@ -5,17 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.onoh.ewallet01.R;
 import com.onoh.ewallet01.activity.BottomNavigationActivity.DompetActivity;
 import com.onoh.ewallet01.activity.BottomNavigationActivity.HistoryActivity;
 import com.onoh.ewallet01.activity.BottomNavigationActivity.PaymentActivity;
 import com.onoh.ewallet01.activity.BottomNavigationActivity.ProfileActivity;
 import com.onoh.ewallet01.fragment.HomeFragment;
-import com.onoh.ewallet01.fragment.ProfileFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +30,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     //inisiasi variabel dengan bindview
     @BindView(R.id.bottom_nav)
     BottomNavigationView bottomNavigationView;
+    @BindView(R.id.shimmer_layout)
+    ShimmerFrameLayout shimmerFrameLayout;
+    //Button menu topbar
+    @BindView(R.id.btn_topup)
+    ImageView btn_topup;
+    @BindView(R.id.btn_transfer)
+    ImageView btn_transfer;
+    @BindView(R.id.btn_terima)
+    ImageView btn_terima;
+    @BindView(R.id.btn_scan_qr)
+    FloatingActionButton btn_scan_qr;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,35 +56,46 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         // beri listener pada saat item/menu bottomnavigation terpilih
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        shimmerFrameLayout.startShimmerAnimation();
+        //set color button qr code
+        btn_scan_qr.setColorFilter(Color.WHITE);
+
+
+        //Intent menu topup,transfer,terima
+        btn_topup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HALAMAN TOPUP", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_transfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HALAMAN TRANSFER", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_terima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HALAMAN TERIMA", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //intent scan qr code
+        btn_scan_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent payment_intent = new Intent(MainActivity.this,PaymentActivity.class);
+                startActivity(payment_intent);
+            }
+        });
 
     }
-//          @Override
-//          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//              switch (item.getItemId()){
-//                  case R.id.home:
-//                      bottomNavigationView.setSelectedItemId(R.id.home);
-//                      return true;
-//                  case R.id.dompet:
-//                      startActivity(new Intent(getApplicationContext(), DompetActivity.class));
-//                      overridePendingTransition(0,0);
-//                      return true;
-//                  case R.id.scanqrcode:
-//                      startActivity(new Intent(getApplicationContext(), PaymentActivity.class));
-//                      overridePendingTransition(0,0);
-//                      return true;
-//                  case R.id.history:
-//                      startActivity(new Intent(getApplicationContext(), HistoryActivity.class));
-//                      overridePendingTransition(0,0);
-//                      return true;
-//                  case R.id.profile:
-//                      startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-//                      overridePendingTransition(0,0);
-//                      return true;
-//              }
-//              return false;
-//          }
-//      });
 
+
+    //Bottom navigasi
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         Fragment fragment = null;
@@ -88,7 +117,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 startActivity(history_intent);
                 break;
             case R.id.profile:
-                fragment = new ProfileFragment();
+                Intent profile_intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(profile_intent);
                 break;
         }
         return loadFragment(fragment);
@@ -104,5 +134,5 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
 
-    
+
 }
